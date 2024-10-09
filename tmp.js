a238af28e112440fdf25f25982ba748ee2f61747
Stef-00012 <stefano.delprete.08@gmail.com>
2024-10-09 18:24:04 +0200

    remove temp test file

--- tmp.js
+++
@@ -1,9 +0,0 @@
-const axios = require('axios');
-
-(async () => {
-    try {
-        await axios.get('https://discord.com/api/v10/users/me')
-    } catch(e) {
-        console.log(e.response.headers.get('content-length'))
-    }
-})()
